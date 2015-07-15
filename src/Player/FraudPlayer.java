a4c2ad8dbf2ef48021db923486ce6e1bab13292a
KangSungHyun <ksh901016@naver.com>
2015-07-15 11:07:58 +0900

    Player 구현

---
+++ src/Player/FraudPlayer.java
@@ -0,0 +1,28 @@
+package Player;
+
+import Dice.Dice;
+import Dice.FraudDice;
+
+public class FraudPlayer extends Player {
+
+	private FraudDice fraudDice;
+	private Player rival;
+
+	FraudPlayer(String name, FraudDice fraudDice, Player rival) {
+		super(name, fraudDice);
+		this.fraudDice = fraudDice;
+		this.rival = rival;
+	}
+
+	@Override
+	public void prepare() {
+		if (getScore() < rival.getScore()) {
+			fraudDice.setMode("strong");
+		} else if (getScore() >= rival.getScore() + 6) {
+			fraudDice.setMode("weak");
+		} else {
+			fraudDice.setMode("normal");
+		}
+	}
+
+}

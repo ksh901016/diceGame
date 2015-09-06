f8607f9f5077163f099bc9063340be20d0fb57a1
KangSungHyun <ksh901016@naver.com>
2015-09-06 14:43:56 +0900

    테스트케이스 작성완료

--- src/Test/DiceTest.java
+++ src/Test/DiceTest.java
@@ -11,14 +11,16 @@ import etc.Recorder;
 
 public class DiceTest {
 	private Dice dice;
-	private Recorder recorder;
-	private FraudDice fDice;
+	private FraudDice Fdice;
 	@Before
 	public void setup(){
 		dice = new Dice();
-		recorder = new Recorder();
-		fDice = new FraudDice();
 		System.out.println("setup ");
+	}
+	@Before
+	public void Fraudsetup(){
+		Fdice = new FraudDice();
+		System.out.println("FraudDice setup");
 	}
 	
 	@SuppressWarnings("deprecation")
@@ -28,12 +30,20 @@ public class DiceTest {
 		Assert.assertTrue(result<=6 &&result>=1);
 	}
 	
-	
 	@Test
-	public void testCheckMode() throws Exception{
-		fDice.setMode("strong");
-		String mode = recorder.checkMode(fDice);
-		Assert.assertEquals("[STRONG]", mode);
-		
+	public void testFraudDice() throws Exception{
+		Fdice.setMode("strong");
+		for(int i =0; i<=1000; i++){
+			int result = Fdice.cast();
+			Assert.assertTrue(result >=2);
+		}
+		//1000번씩 테스트
+		Fdice.setMode("weak");
+		for(int i=0; i<=100; i++){
+			int result = Fdice.cast();
+			Assert.assertTrue(result<=5);
+		}
 	}
+	
+	
 }
